433377df37aa9e1719dd04780228ae6288cf75a7
Ian Morland <ian@morland.me>
2022-01-25 10:33:19 +0000

    Use ItemList, format js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,13 +1,12 @@
 import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import NotificationList from 'flarum/forum/components/NotificationList';
-import { findFirstVdomChild } from './util/findVdomChild';
 import Tooltip from 'flarum/common/components/Tooltip';
 import Button from 'flarum/common/components/Button';
 import NotificationListState from 'flarum/forum/states/NotificationListState';
 
 app.initializers.add('blomstra/notification-deleter', () => {
-  NotificationListState.prototype.deleteAll = function() {
+  NotificationListState.prototype.deleteAll = function () {
     if (this.pages.length === 0) return;
 
     app.session.user?.pushAttributes({ unreadNotificationCount: 0 });
@@ -20,27 +19,23 @@ app.initializers.add('blomstra/notification-deleter', () => {
     });
   };
 
-  extend(NotificationList.prototype, 'view', function (vnode) {
-    const state = this.attrs.state;
-
-    findFirstVdomChild(vnode, '.App-primaryControl', function (vnode) {
-      if (!Array.isArray(vnode.children)) vnode.children = [vnode.children];
-
-      vnode.children.push(
-        <Tooltip text={app.translator.trans('blomstra-notification-deleter.forum.notifications.delete_all_tooltip')}>
-          <Button
-            className="Button Button--link"
-            data-container=".NotificationList"
-            icon="fas fa-trash-alt"
-            title={app.translator.trans('blomstra-notification-deleter.forum.notifications.delete_all_tooltip')}
-            onclick={() => {
-              if (confirm(app.translator.trans('blomstra-notification-deleter.forum.notifications.delete_all_confirm'))) {
-                state.deleteAll.call(state);
-              }
-            }}
-          />
-        </Tooltip>
-      );
-    });
+  extend(NotificationList.prototype, 'controlItems', function (items) {
+    items.add(
+      'delete',
+      <Tooltip text={app.translator.trans('blomstra-notification-deleter.forum.notifications.delete_all_tooltip')}>
+        <Button
+          className="Button Button--link"
+          data-container=".NotificationList"
+          icon="fas fa-trash-alt"
+          title={app.translator.trans('blomstra-notification-deleter.forum.notifications.delete_all_tooltip')}
+          onclick={() => {
+            if (confirm(app.translator.trans('blomstra-notification-deleter.forum.notifications.delete_all_confirm'))) {
+              state.deleteAll.call(state);
+            }
+          }}
+        />
+      </Tooltip>,
+      60
+    );
   });
 });
